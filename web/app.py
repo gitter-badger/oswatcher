5af1e6c6f5fa3ae0d483fe36f861e7d85096c36d
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-05-22 20:16:55 +0300

    add Bootstrap

--- web/app.py
+++ web/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 app.config.from_object('config')
+Bootstrap(app)
 
 @app.route('/')
 def main():
